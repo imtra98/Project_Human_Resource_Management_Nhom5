50e50335061daee1d178cf44f4441f4060f49226
imtra98 <123044561+imtra98@users.noreply.github.com>
2023-03-17 23:40:22 +0700

    Update DB_HRManage.sql

--- Database/DB_HRManage.sql
+++ Database/DB_HRManage.sql
@@ -1,20 +1,3 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               8.0.31 - MySQL Community Server - GPL
--- Server OS:                    Win64
--- HeidiSQL Version:             12.4.0.6659
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
 -- Dumping database structure for human_resource_management
 CREATE DATABASE IF NOT EXISTS `human_resource_management` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `human_resource_management`;
@@ -87,8 +70,3 @@ INSERT INTO `user` (`userId`, `username`, `userCode`, `password`, `fullName`) VA
 	('5329b76b-d244-4ef1-9deb-75babf5bba69', 'admin2', 'Ad002', '090998', 'Nguyễn Thị Thu Trà'),
 	('c73e8eb5-daca-49e5-9ec8-8409669b1431', 'admin1', 'Ad001', '240599', 'Lưu Tiến Hoan');
 
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
